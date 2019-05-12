2419aae61ea23c92c0d524d1698bcec62ad816b4
Yonas Adiel <yonazadielwiguna@gmail.com>
2019-05-12 10:41:15 +0700

    Updates

--- src/components/PageComponent.js
+++
@@ -1,7 +0,0 @@
-import { Component } from "react";
-
-export default class PageComponent extends Component {
-  componentDidMount() {
-    window.scrollTo(0, 0);
-  }
-}
--- src/routes/index.js
+++
@@ -1,1 +0,0 @@
-exports.ROUTE_HOME = '/';
